5f196d4dbb0bf225588f0337a88f861d1b1d5a2d
dujinqiao <duzi1996@qq.com>
2017-12-14 11:29:21 +0800

    add to gh-pages

---
+++ src/components/TodoList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import TodoItems from './TodoItems';
+
+export default class TodoList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: JSON.parse(localStorage.getItem("items")) || [],
+        };
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e) {
+        let items = this.state.items;
+        let input = this.refs.input.value;
+        if (input) {
+            items.unshift({
+                text: input,
+                key: Date.now(),
+            });
+
+            this.setState({items});
+            this.refs.input.value = '';
+        }
+
+        localStorage.setItem("items", JSON.stringify(this.state.items));
+
+        e.preventDefault();
+    }
+
+    deleteItem(key) {
+        let filtedItems = this.state.items.filter(item => item.key !== key);
+
+        this.setState({
+            items: filtedItems,
+        });
+
+        localStorage.setItem("items", JSON.stringify(this.state.items));
+    }
+
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input type="text" ref="input" placeholder="input task"/>
+                        <button type="submit">add</button>
+                    </form>
+                    <TodoItems items={this.state.items}
+                               delete={this.deleteItem}/>
+                </div>
+            </div>
+        );
+    }
+}
